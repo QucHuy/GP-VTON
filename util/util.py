687c5964aeed49680d6c6d6c4d5737459f40b110
ecgit <quochuy030702@gmail.com>
2023-11-05 17:22:47 +0700

    add para in util

--- util/util.py
+++ util/util.py
@@ -62,7 +62,7 @@ def mkdirs(paths):
 
 def mkdir(path):
     if not os.path.exists(path):
-        os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
 
 ###############################################################################
 # Code from
